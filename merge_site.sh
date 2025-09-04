3384bb9fd6855482ae1d0448e8a81d2bbaba3207
pmbryant <35501945+pmbryant@users.noreply.github.com>
2025-09-03 22:45:21 -0500

    Github action and script to build site for GitHub Pages

---
+++ merge_site.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+DEST_DIR="docs"
+
+# Remove all contents of destination directory if it exists
+if [ -d "$DEST_DIR" ]; then
+    rm -rf "$DEST_DIR"/*
+fi
+
+# Create the destination directory if it doesn't exist
+mkdir -p "$DEST_DIR"
+
+# Function to merge subdirectories from a source into destination
+merge_subdirs() {
+    src_dir="$1"
+    for sub in "$src_dir"/*; do
+        if [ -d "$sub" ]; then
+            subname=$(basename "$sub")
+            # Rename pmbryant.typepad.com to x
+            if [ "$subname" = "pmbryant.typepad.com" ]; then
+                subname="x"
+            fi
+            mkdir -p "$DEST_DIR/$subname"
+            cp -a "$sub"/. "$DEST_DIR/$subname/"
+        fi
+    done
+}
+
+# Merge subdirs under bb-blog
+merge_subdirs "bb-blog"
+
+# Merge subdirs under lyg-blog
+merge_subdirs "lyg-blog"
+
+# Copy contents of docs_permanent into destination directory
+if [ -d "docs_permanent" ]; then
+    cp -a docs_permanent/. "$DEST_DIR/"
+fi
+
+echo "Merge complete."
